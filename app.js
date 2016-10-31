ba01c70f35f4ab34f46a448c0e3e97c860edeb27
Keith Magill <kmagi@allstate.com>
2016-10-31 15:13:55 +0000

    more on bootstrap

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var profiles = require('./routes/profiles')
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/profiles', profiles);
 app.use('/', routes);
 app.use('/users', users);
 
